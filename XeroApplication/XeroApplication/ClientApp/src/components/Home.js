6ea3de1c2168578925b4565e13721b7d913fc9ce
Dion Bedford <bedfdc1@student.op.ac.nz>
2020-09-24 13:37:28 +1200

    Updated home page to hero welcome message, Removed unessasary sidebar files

--- XeroApplication/XeroApplication/ClientApp/src/components/Home.js
+++ XeroApplication/XeroApplication/ClientApp/src/components/Home.js
@@ -5,22 +5,19 @@ export class Home extends Component {
 
   render () {
     return (
-      <div className="landing-page container">
-        <h1>Welcome to Xero-Ninja!</h1>
-        <p>Your One Stop Solution for compiling all your account information in on easy dashboard!</p>
-        <img href='.\flask-dashboard-coreui-screen.png' />
-        {/* <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p> */}
+      <div>
+        <div class="jumbotron align-middle">
+          <h1 class="display-4">Welcome to Xero-Ninja!</h1>
+          <p class="lead">Your One Stop Solution for compiling all your account information in on easy dashboard!</p>
+          <hr class="my-4"/>
+          <img className="img-fluid" src={process.env.PUBLIC_URL + '/img/dash.jpg'} />
+          <p className="heroText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel molestie enim. Nulla ornare eros risus, at aliquam sapien convallis pulvinar. Cras eu maximus nulla, vitae faucibus lorem. Vestibulum odio mauris, molestie convallis quam vel, tempor ultricies massa. Nulla et magna at turpis vulputate venenatis at vel tortor. Aliquam est ex, cursus id sagittis et, varius placerat urna. Quisque vehicula lacus in nunc faucibus pharetra. Suspendisse tempor nibh ac sodales pellentesque. Morbi dignissim nec nulla ut tincidunt. Aenean laoreet sem vel commodo vulputate. Aenean pharetra massa vitae arcu fermentum tincidunt. Mauris interdum iaculis odio, vel efficitur purus pulvinar quis. Pellentesque congue ultricies elit a ullamcorper.
+          Vestibulum eu metus sollicitudin, vestibulum erat non, malesuada eros. Suspendisse nec mauris sollicitudin, tincidunt orci in, consequat eros. Nulla facilisi. Suspendisse potenti. Aliquam et pulvinar sapien. Quisque cursus condimentum magna, non feugiat est convallis non. Ut diam tellus, tincidunt a augue et, maximus aliquet nulla. Suspendisse mattis sed sem vitae iaculis. Suspendisse potenti. Etiam hendrerit eros quis aliquam dignissim.</p>
+          <hr class="my-4"/>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Get Started!</a>
+          </p>
+        </div>
       </div>
     );
   }
