163e14f475dacbc9c20c350554502d499f800487
Dion Bedford <bedfdc1@student.op.ac.nz>
2020-10-27 12:39:21 +1300

    Started installing recharts

--- XeroApplication/XeroApplication/ClientApp/src/components/FetchData.js
+++ XeroApplication/XeroApplication/ClientApp/src/components/FetchData.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import authService from './api-authorization/AuthorizeService'
-import {CCard, CCardBody, CCardGroup, CCardHeader} from '@coreui/react'
-import {CChartDoughnut, CChartLine, CChartRadar} from '@coreui/react-chartjs'
+import React, { Component,PureComponent } from 'react';
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import { FaHeart,FaGem } from 'react-icons/fa';
+import { FaGitter } from 'react-icons/fa';
 import 'react-pro-sidebar/dist/css/styles.css';
-
+import {
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
 export class FetchData extends Component {
   static displayName = FetchData.name;
@@ -76,19 +75,10 @@ export class FetchData extends Component {
       });
   }
 
-  // static renderForecastsTable(forecasts) {
-  //   //console.log(forecasts);
-  //   return (
-  //     <div>
-  //       <p>{forecasts.status}</p>
-  //       <p>{forecasts.link}</p>
-  //     </div>
-  //   );
-  // }
 
   render() {
     //let contents = this.state.loading ? <p><em>Loading...</em></p> : FetchData.renderForecastsTable(this.state.forecasts); 
-    
+    const {data} = this.state;
     return (
         <div>
           <div className="container dasboardContainer">
@@ -96,145 +86,29 @@ export class FetchData extends Component {
               <div className="col">
                 <ProSidebar>
                   <Menu iconShape="square">
-                    <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-                    <SubMenu title="Components" icon={<FaHeart />}>
-                      <MenuItem>Component 1</MenuItem>
-                      <MenuItem>Component 2</MenuItem>
-                    </SubMenu>
+                    <MenuItem icon={<FaGitter />}>Invoices</MenuItem>
                   </Menu>
                 </ProSidebar>
+                <div className="col-10">
+                  <BarChart
+                  width={700}
+                  height={500}
+                  data={data}
+                  margin={{
+                    top: 5, right: 30, left: 20, bottom: 5,
+                  }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="InvoiceNumber" />
+                    <YAxis dataKey="" />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="Total" fill="#82ca9d" />
+                  </BarChart>
               </div>
-              <div className="col-10">
-              <button onClick={this.XeroAuthSend}>Xero Connect</button>
-                <CCardGroup>
-                  <CCard>
-                    <CCardHeader>
-                      Line Chart
-                    </CCardHeader>
-                    <CCardBody>
-                      <CChartLine
-                        datasets={[
-                          {
-                            label: 'Data One',
-                            backgroundColor: 'rgb(228,102,81,0.9)',
-                            data: [30, 39, 10, 50, 30, 70, 35]
-                          },
-                          {
-                            label: 'Data Two',
-                            backgroundColor: 'rgb(0,216,255,0.9)',
-                            data: [39, 80, 40, 35, 40, 20, 45]
-                          }
-                        ]}
-                        options={{
-                          tooltips: {
-                            enabled: true
-                          }
-                        }}
-                        labels="months"
-                      />
-                    </CCardBody>
-                  </CCard>
-                </CCardGroup>
-
-                <CCardGroup columns className="cols-2">
-                
-                <CCard>
-                  <CCardHeader>
-                    Doughnut Chart
-                  </CCardHeader>
-                  <CCardBody>
-                    <CChartDoughnut
-                      datasets={[
-                        {
-                          backgroundColor: [
-                            '#41B883',
-                            '#E46651',
-                            '#00D8FF',
-                            '#DD1B16'
-                          ],
-                          data: [40, 20, 80, 10]
-                        }
-                      ]}
-                      labels={['VueJs', 'EmberJs', 'ReactJs', 'AngularJs']}
-                      options={{
-                        tooltips: {
-                          enabled: true
-                        }
-                      }}
-                    />
-                  </CCardBody>
-                </CCard>
-
-                <CCard>
-                  <CCardHeader>
-                    Radar Chart
-                  </CCardHeader>
-                  <CCardBody>
-                    <CChartRadar
-                      datasets={[
-                        {
-                          label: '2019',
-                          backgroundColor: 'rgba(179,181,198,0.2)',
-                          borderColor: 'rgba(179,181,198,1)',
-                          pointBackgroundColor: 'rgba(179,181,198,1)',
-                          pointBorderColor: '#fff',
-                          pointHoverBackgroundColor: '#fff',
-                          pointHoverBorderColor: 'rgba(179,181,198,1)',
-                          tooltipLabelColor: 'rgba(179,181,198,1)',
-                          data: [65, 59, 90, 81, 56, 55, 40]
-                        },
-                        {
-                          label: '2020',
-                          backgroundColor: 'rgba(255,99,132,0.2)',
-                          borderColor: 'rgba(255,99,132,1)',
-                          pointBackgroundColor: 'rgba(255,99,132,1)',
-                          pointBorderColor: '#fff',
-                          pointHoverBackgroundColor: '#fff',
-                          pointHoverBorderColor: 'rgba(255,99,132,1)',
-                          tooltipLabelColor: 'rgba(255,99,132,1)',
-                          data: [28, 48, 40, 19, 96, 27, 100]
-                        }
-                      ]}
-                      options={{
-                        aspectRatio: 1.5,
-                        tooltips: {
-                          enabled: true
-                        }
-                      }}
-                      labels={[
-                        'Eating', 'Drinking', 'Sleeping', 'Designing',
-                        'Coding', 'Cycling', 'Running'
-                      ]}
-                    />
-                  </CCardBody>
-                  </CCard>
-              </CCardGroup>
             </div>
           </div>
         </div>
       </div>
     );
   }
-
-
-
-  // async populateWeatherData() {
-  //   const token = await authService.getAccessToken();
-  //   const response = await fetch('weatherforecast', {
-  //     headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
-  //   });
-  //   const data = await response.json();
-  //   this.setState({ forecasts: data, loading: false });
-  // }
-  
-  // async populateWeatherData() {
-  //   // const token = await authService.getAccessToken();
-  //   // const response = await fetch('https://localhost:5001/get-link', {
-  //   //   headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
-  //   // });
-  //   const response = await fetch('https://localhost:5001/get-link')
-  //   .then(response => response.json())
-  //   //const data = await response.json();
-  //   this.setState({ forecasts: response, loading: false });
-  // }
 }
