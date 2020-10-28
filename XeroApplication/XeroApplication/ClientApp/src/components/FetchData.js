dfea6c356e00fe85c8f1501211a0a60f27063cd7
Dion Bedford <bedfdc1@student.op.ac.nz>
2020-10-29 12:40:30 +1300

    Added bar graph to show latest invoices and their totals

--- XeroApplication/XeroApplication/ClientApp/src/components/FetchData.js
+++ XeroApplication/XeroApplication/ClientApp/src/components/FetchData.js
@@ -3,7 +3,7 @@ import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { FaGitter } from 'react-icons/fa';
 import 'react-pro-sidebar/dist/css/styles.css';
 import {
-  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer,
 } from 'recharts';
 
 export class FetchData extends Component {
@@ -11,7 +11,7 @@ export class FetchData extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { forecasts: [],  loading: true };
+    this.state = { data: [], forecasts: [],  loading: true };
     this.gettoken = { data: [], loading: true };
     this.getdata = { data: [], loading: true };
     this.XeroAuthSend = this.XeroAuthSend.bind(this);
@@ -43,7 +43,6 @@ export class FetchData extends Component {
   })
   .then(response => response.json())
     .then((data) => {
-      console.log(data);
       this.state = { forecasts: data, loading: false };
     });
   }
@@ -57,7 +56,6 @@ export class FetchData extends Component {
     })
     .then(response => response.json())
       .then((dataToken) => {
-        console.log(dataToken);
         this.gettoken = { data: dataToken, loading: false };
       });
   }
@@ -70,45 +68,44 @@ export class FetchData extends Component {
     })
     .then(response => response.json())
       .then((dataXero) => {
-        console.log(dataXero);
-        this.getdata = { data: dataXero, loading: false };
+        this.setState({ data: dataXero, loading: false });
       });
   }
 
 
   render() {
-    //let contents = this.state.loading ? <p><em>Loading...</em></p> : FetchData.renderForecastsTable(this.state.forecasts); 
     const {data} = this.state;
     return (
         <div>
           <div className="container dasboardContainer">
             <div className="row">
-              <div className="col">
+              <div className="col-2">
                 <ProSidebar>
                   <Menu iconShape="square">
                     <MenuItem icon={<FaGitter />}>Invoices</MenuItem>
                   </Menu>
                 </ProSidebar>
-                <div className="col-10">
-                  <BarChart
-                  width={700}
-                  height={500}
-                  data={data}
-                  margin={{
-                    top: 5, right: 30, left: 20, bottom: 5,
-                  }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="InvoiceNumber" />
-                    <YAxis dataKey="" />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="Total" fill="#82ca9d" />
-                  </BarChart>
+              </div>
+              <div className="col-10 barChart">
+                <h1>Invoices</h1>
+                <ResponsiveContainer width='90%' height={600}>
+                    <BarChart
+                    data={data.Invoices}
+                    margin={{
+                      top: 5, right: 30, left: 20, bottom: 5,
+                    }}>
+                      <CartesianGrid strokeDasharray="3 3" stroke="#ffffff"/>
+                      <XAxis dataKey="date" stroke="#ffffff"/>
+                      <YAxis/>
+                      <Tooltip dataKey="date" stroke="#ffffff"/>
+                      <Legend width={100} wrapperStyle={{left:500, Color: '#ffff', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+                      <Bar dataKey="Total" fill="#82ca9d" />
+                    </BarChart>
+                  </ResponsiveContainer>
               </div>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
